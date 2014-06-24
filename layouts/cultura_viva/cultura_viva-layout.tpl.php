90b58223a45a10575bba67755d46c7730476c8c2
dpineiden <dahalpi@gmail.com>
2014-06-23 22:14:04 -0400

    Menu central y formulario

--- layouts/cultura_viva/cultura_viva-layout.tpl.php
+++ layouts/cultura_viva/cultura_viva-layout.tpl.php
@@ -42,10 +42,10 @@
       <?php print $messages; ?>
       <?php print render($tabs); ?>
       <?php print render($page['help']); ?>
+      <?php print render($page['content']); ?>
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
-      <?php print render($page['content']); ?>
       <?php print $feed_icons; ?>  
     </div>
   <?php print render($page['sidebar']); ?>
